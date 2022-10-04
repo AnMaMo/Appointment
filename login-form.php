964a248c86c68066a51b4ae5cf9494ed6f9bd85e
srlin02 <pinlinlinqiu2002@gmail.com>
2022-10-04 23:04:38 +0200

    login fet

---
+++ login-form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login-form</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="form-contenedor">
+        <div class="form-title central">Login</div>
+        <div class="form-square-form "> 
+            <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nom usuari</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contraseya</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">iniciar sessió</button>
+            </form>
+        </div>
+    </div>
+    
+    
+
+</body>
+</html>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
