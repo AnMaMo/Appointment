f4d9b912b81331870096cd6014aba5a7f7892a9e
srlin02 <pinlinlinqiu2002@gmail.com>
2022-10-20 18:52:57 +0200

    esborrany1.2

--- src/views/user-form.php
+++ src/views/user-form.php
@@ -16,65 +16,76 @@ $username = "Pinlin";
 ?>
 
 <body>
-
+    <!--buscador-->
     <div class="container">
-        <form class="row align-items-start">
-            <div class="col">
-                <input type="text" class="form-control" id="searchUser" placeholder="Search user">
-            </div>
-            <div class="col">
-                <button type="submit" class="btn btn-primary mb-3"><img class="search-logo" src="../media/search.png" alt=""></button>
+        <form class="row centre">
+            <div class="col user-margin">
+                <input type="text" class="user-inputbox centre" id="searchUser" placeholder="Search user">
+                <button type="reset" class="user-buttom-search"><img class="search-logo" src="../media/search.png" alt="search"></button>
             </div>
         </form>
     </div>
 
+    <!-- Canvi de rol -->
     <div class="container">
         <form action="">
             <div class="row align-items-start">
-                <div class="col centre  ">
-                    <h1><?= $username ?></h1>
-                </div>
-                <div class="col">
-                    <select class="form-select" aria-label="Default select example">
+                <div class="col centre user-square ">
+                    <p><?= $username ?></p>
+                    <!--
+                    -->
+                    <select class="form-select centre" aria-label="Default select example">
                         <option selected>Tria el rol</option>
                         <option value="admin">Admin</option>
+                        <option value="gestor">Gestor</option>
                         <option value="user">User</option>
                     </select>
+                    <div class="col centre user-square user-margin">
+                        <h1>Appointment</h1>
+                        <div class="row ">
+
+                            <div class="col">
+                                <p>cita1</p>
+                                <button type="button" class="btn btn-danger">Cancelar</button>
+
+                            </div>
+                            <div class="col">
+                                <p>cita2</p>
+                                <button type="button" class="btn btn-danger">Cancelar</button>
+
+                            </div>
+                            <div class="col">
+                                <p>cita3</p>
+                                <button type="button" class="btn btn-danger">Cancelar</button>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col centre user-margin user-square">
+                        <div>
+                            <h1>User info</h1>
+
+                        </div>
+
+                        <div>
+                            <p>hola</p>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </form>
     </div>
 
-    <div class="container user-margin">
-        <div class="row align-items-start">
-            <div class="col centre">
-                <h1>Appointment</h1>
-                <div class="row">
-                    <div class="col">
-                        <p>hola</p>
-                    </div>
-                    <div class="col">
-                        <p>hola2</p>
-                    </div>
-                    <div class="col">
-                        <p>hola3</p>
-                    </div>
-                </div>
 
+    <!--<div class="container user-margin">
+        <form action="">
+            <div class="row align-items-start">
+                
             </div>
-
-            <div class="col centre">
-                <div>
-                    <h1>User info</h1>
-
-                </div>
-
-                <div> 
-                    <p>hola</p>
-                </div>
-            </div>
-        </div>
-    </div>
+        </form>
+    </div> -->
 
 </body>
 
