6e9f20a9c115b0d1721a36c40a1ef2ac6d738b8d
Andre <andremartinezmo@gmail.com>
2022-10-21 15:55:06 +0200

    Framework ok and login y register

--- src/views/register-form.php
+++ src/views/register-form.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,27 +9,52 @@
     <link rel="stylesheet" href="style.css">
 
 </head>
+
 <body>
-  <div class="form-contenedor">
-    
-    <span class="square"></span> <div class="form-title">Register</div>
-    <div class="form-square-form "> 
-        <form class="all-forms" method="post" action="register.php">
-        <div class="mb-3">
-            <input type="text" class="form-control" id="login-name" placeholder="Username" name="username" required>
+    <?php include '../src/views/templates/navbar.php'; ?>
+
+    <div class="form-contenedor">
+
+        <span class="square"></span>
+        <div class="form-title">Register</div>
+        <div class="form-square-form ">
+            <form class="all-forms" method="post" action="index.php">
+                <input type="hidden" name="page" value="doregister">
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="login-name" placeholder="Username" name="username" required>
+                </div>
+
+                <div class="mb-3">
+                    <input type="email" class="form-control" id="login-mail" aria-describedby="emailHelp" placeholder="Mail" name="mail" required>
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="login-password" placeholder="Password" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
         </div>
-        <div class="mb-3">
-            <input type="email" class="form-control" id="login-mail" aria-describedby="emailHelp" placeholder="Mail"  name="mail" required>
+
+
+        <div class="subform">
+            <?php
+            if (isset($_GET['userexist'])) {
+            ?>
+                <div class="incorrect-credentials" id="credentials-error">
+                    <p>This username already exist!</p>
+                </div>
+            <?php
+            }
+            ?>
+            <div class="button"><a href="">Signup</a></div>
         </div>
-        <div class="mb-3">
-            <input type="password" class="form-control" id="login-password" placeholder="Password" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Register</button>
-        </form>
+
+
     </div>
-    <div class="button"><a href="">Signup</a></div>
-  </div>
+    <?php include '../src/views/templates/footer.php'; ?>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
