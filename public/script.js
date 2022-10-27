789019fb469149675579e2ea4b6c620d2960af80
Andre <andremartinezmo@gmail.com>
2022-10-27 19:50:58 +0200

    appointment for hours and workstations its ok

--- public/script.js
+++ public/script.js
@@ -1,31 +1,57 @@
+/**
+ * This function set the params of the datepicker JQuery
+ */
 $(function datepicker() {
     $("#datepicker").datepicker({ dateFormat: "yy/mm/dd" }).val();
 });
 
+/**
+ * Function to see if user has entered an hour
+ */
+function appointmentHourValidation(){
+    var hour = $("#hour_selected").val();
+    if (hour == "") {
+        alert("Please select an hour");
+        return false;
+    }
+}
 
 /**
  * Function to call ajax to get the hours available for the selected date
  */
 function selectDate(date) {
-    //get the workstation id and the date selected
-    var workstation = document.getElementById("workstation").value;
-    var date = document.getElementById("datepicker").value;
+    // Get the workstation id and the date selected
+    var workstation = $("#workstation").val();
+    var date = $("#datepicker").val();
 
+    /**
+     * CALL AJAX
+     * url: call a controller to get the no avaliable hours for the selected date
+     * type: POST
+     * data: date and workstation id (send to the controller)
+     * dataType: JSON
+     * success: call the function ProcessInvalidHours when the ajax is success
+     */
     $.ajax({
         url: 'index.php?page=checkhours',
         type: 'POST',
         data: { date: date, workstation: workstation },
         dataType: "json",
-        success: procesJSON
+        success: ProcessInvalidHours
     });
 }
 
-function procesJSON(data) {
+/**
+ * Function to process the hours not available for the selected date and workstation
+ * @param {*} data 
+ */
+function ProcessInvalidHours(data) {
+    // Reset the hour clicked and the input "hour_selected"
+    $(".hourclicked").removeClass("hourclicked").addClass("hour");
+    $("#hour_selected").val("");
 
-//get all elements with clas novalidhour and set attribute onclick with jquery
-$(".hourclicked").removeClass("hourclicked").addClass("hour");
+    // Set the click attribute on a previous hours selected and set valid again
     $(".noValidHour").attr("onclick", "clickHour(this)");
-
     $(".noValidHour").removeClass("noValidHour").addClass("hour");
 
     // Iterate the elements of the JSON array
@@ -33,7 +59,7 @@ $(".hourclicked").removeClass("hourclicked").addClass("hour");
         // Save the date in a datetime variable
         var dates = new Date(date.app_datetime);
 
-        //get hour and minutes to the datetime
+        // Get hour and minutes to the datetime
         var hour = dates.getHours();
         var minutes = dates.getMinutes();
         if (minutes == 0) {
@@ -43,47 +69,38 @@ $(".hourclicked").removeClass("hourclicked").addClass("hour");
         // Save the no valid hours in a variable
         var novalidHour = hour + ":" + minutes;
 
-        console.log(novalidHour);
-
-        //get element by name novaidHour and set new class
+        // Get element by name novaidHour and set new class
         var element = document.getElementsByName(novalidHour);
 
-        element[0].classList.remove("hour");
-        element[0].classList.add("noValidHour");
-
-        //remove onclick event from the no valid hours
-        element[0].removeAttribute("onclick");
+        // Put the hour to no valid and and remove click attribute
+        $(element[0]).removeClass("hour").addClass("noValidHour");
+        $(element[0]).removeAttr("onclick");
     }
-
 }
 
 
-
-
-
+/**
+ * Function executed when the user clicks on an hour.
+ * @param {*} hourclicked 
+ */
 function clickHour(hourclicked) {
-    //get all elements with class "hour-clicked"
-    var elements = document.getElementsByClassName("hourclicked");
-    //remove class "hour-clicked" from all elements
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.add("hour");
-        elements[i].classList.remove("hourclicked");
-    }
+    // Get all elements with class hourclicked and set new class hour.
+    $(".hourclicked").removeClass("hourclicked").addClass("hour");
 
-    //get element with id "hourclicked" and set new class
-    hourclicked.classList.remove("hour");
-    hourclicked.classList.add("hourclicked");
+    // Get the element clicked and set new class hourclicked.
+    $(hourclicked).removeClass("hour").addClass("hourclicked");
 
-    //set to input "hour_selected" the value of the clicked hour
+    // Set to input "hour_selected" the value of the clicked hour.
     var hour = $('.hourclicked').attr('name');
-    document.getElementById("hour_selected").value = hour;
+    $("#hour_selected").val(hour);
 }
 
 
-
+/**
+ * Function to see if user has entered invalid credentials
+ * in the login form and hide the error message
+ */
 function invalidCredentials() {
-    //get element and set new class
-    var element = document.getElementById("credentials-error");
-    element.classList.add("invalid-credentials");
-    element.classList.remove("correct-credentials");
+    // Hidde the error message
+    $(".credentials-error").removeClass("correct-credentials").addClass("invalid-credentials");
 }
--- src/views/appointment-form.php
+++ src/views/appointment-form.php
@@ -30,7 +30,8 @@
                     <form action="index.php" method="POST" class="all-forms" name="appointmentForm">
                         <input type="hidden" name="page" value="doAppointment">
                         <div class="mb-3">
-                            <input type="text" id="datepicker" name="dateAppointment" placeholder="Select a date" onchange="selectDate()">
+                            <div class="dateAndWorkstation">
+                            <input type="text" id="datepicker" name="dateAppointment" placeholder="Select a date" onchange="selectDate()" required>
                             <select id="workstation" name="workstation_selected" onchange="selectDate()">
                                 <?php
                                 //Get the workstations of resposta
@@ -43,36 +44,37 @@
 
                                 ?>
                             </select>
+                            </div>
                         </div>
 
                         <div class="hour_selector">
-                        <div class="hours-div">
-                            <div class="hour" id="hour1" name="9:00" onclick="clickHour(this)">9:00-9:30</div>
-                            <div class="hour" id="hour2" name="9:30" onclick="clickHour(this)">9:30-10:00</div>
-                            <div class="hour" id="hour3" name="10:00" onclick="clickHour(this)">10:00-10:30</div>
-                            <div class="hour" id="hour4" name="10:30" onclick="clickHour(this)">10:30-11:00</div>
-                        </div>
-                        <div class="hours-div">
-                            <div class="hour" id="hour5" name="11:00" onclick="clickHour(this)">11:00-11:30</div>
-                            <div class="hour" id="hour6" name="11:30" onclick="clickHour(this)">11:30-12:00</div>
-                            <div class="hour" id="hour7" name="12:00" onclick="clickHour(this)">12:00-12:30</div>
-                            <div class="hour" id="hour8" name="12:30" onclick="clickHour(this)">12:30-13:00</div>
-                        </div>
-                        <div class="hours-div">
-                            <div class="hour" id="hour9" name="16:00" onclick="clickHour(this)">16:00-16:30</div>
-                            <div class="hour" id="hour10" name="16:30" onclick="clickHour(this)">16:30-17:00</div>
-                            <div class="hour" id="hour11" name="17:00" onclick="clickHour(this)">17:00-17:30</div>
-                            <div class="hour" id="hour12" name="17:30" onclick="clickHour(this)">17:30-18:00</div>
-                        </div>
-                        <div class="hours-div">
-                            <div class="hour" id="hour13" name="18:00" onclick="clickHour(this)">18:00-18:30</div>
-                            <div class="hour" id="hour14" name="18:30" onclick="clickHour(this)">18:30-19:00</div>
-                            <div class="hour" id="hour15" name="19:00" onclick="clickHour(this)">19:00-19:30</div>
-                            <div class="hour" id="hour16" name="19:30" onclick="clickHour(this)">19:30-20:00</div>
-                        </div>
+                            <div class="hours-div">
+                                <div class="hour" id="hour1" name="9:00" onclick="clickHour(this)">9:00-9:30</div>
+                                <div class="hour" id="hour2" name="9:30" onclick="clickHour(this)">9:30-10:00</div>
+                                <div class="hour" id="hour3" name="10:00" onclick="clickHour(this)">10:00-10:30</div>
+                                <div class="hour" id="hour4" name="10:30" onclick="clickHour(this)">10:30-11:00</div>
+                            </div>
+                            <div class="hours-div">
+                                <div class="hour" id="hour5" name="11:00" onclick="clickHour(this)">11:00-11:30</div>
+                                <div class="hour" id="hour6" name="11:30" onclick="clickHour(this)">11:30-12:00</div>
+                                <div class="hour" id="hour7" name="12:00" onclick="clickHour(this)">12:00-12:30</div>
+                                <div class="hour" id="hour8" name="12:30" onclick="clickHour(this)">12:30-13:00</div>
+                            </div>
+                            <div class="hours-div">
+                                <div class="hour" id="hour9" name="16:00" onclick="clickHour(this)">16:00-16:30</div>
+                                <div class="hour" id="hour10" name="16:30" onclick="clickHour(this)">16:30-17:00</div>
+                                <div class="hour" id="hour11" name="17:00" onclick="clickHour(this)">17:00-17:30</div>
+                                <div class="hour" id="hour12" name="17:30" onclick="clickHour(this)">17:30-18:00</div>
+                            </div>
+                            <div class="hours-div">
+                                <div class="hour" id="hour13" name="18:00" onclick="clickHour(this)">18:00-18:30</div>
+                                <div class="hour" id="hour14" name="18:30" onclick="clickHour(this)">18:30-19:00</div>
+                                <div class="hour" id="hour15" name="19:00" onclick="clickHour(this)">19:00-19:30</div>
+                                <div class="hour" id="hour16" name="19:30" onclick="clickHour(this)">19:30-20:00</div>
+                            </div>
                         </div>
                         <input type="text" class="hidden" name="hour_selected" id="hour_selected" required>
-                        <button type="submit" class="btn-red btn-primary">Take Appointment</button>
+                        <button type="submit" class="btn-red btn-primary" onclick="appointmentHourValidation()">Take Appointment</button>
                     </form>
                 </div>
             </div>
